5660ed4fd5fd2716db6a87074d7cfe2e5505ee35
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2024-12-16 00:18:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const makeChange = (c) => {
   // your name here
+	const obj={
+		q:0,
+		d:0,
+		n:0,
+		p:0
+	};
+	while(c){
+		if(c>=25){
+			obj.q++;
+			c-=25;
+		}else if(c<25 && c>=10){
+			obj.d++;
+			c-=10;
+		}else if(c<10 && c>=5){
+			obj.n++;
+			c-=5;
+		}else{
+			obj.p++;
+			c-=1;
+		}
+	}
+	return obj;
 };
 
 // Do not the change the code below
